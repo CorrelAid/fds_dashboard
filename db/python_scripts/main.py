4fb8cb20c9f4f1651f1134c88f65fa37755f84bc
Jonas Stettner <mail@jstet.net>
2023-04-17 18:19:27 +0200

    dl message though foi requests

--- db/python_scripts/main.py
+++ db/python_scripts/main.py
@@ -4,7 +4,7 @@ from helpers import dload
 
 console = Console()
 
-cols_foi_requests = ["id", "jurisdiction", "refusal_reason", "costs", "due_date", "resolved_on", "created_at", "last_message", "status", "resolution", "user", "public_body", "campaign"]
+cols_foi_requests = ["id", "jurisdiction", "refusal_reason", "costs", "due_date", "resolved_on", "created_at", "last_message", "status", "resolution", "user", "public_body", "campaign","messages"]
 cols_messages = ["id", "request", "sent", "is_response", "is_postal", "kind", "sender_public_body", "recipient_public_body", "status", "timestamp"]
 cols_pbodies = ["id", "name", "classification", "categories", "address", "jurisdiction", ]
 cols_campaigns=["id", "name", "slug", "url", "description", "start_date", "active"]
@@ -17,13 +17,13 @@ console.log("Beginning to download FOI requests.")
 dload("https://fragdenstaat.de/api/v1/request/", "foi_requests", "last_message", cols_foi_requests, console)
 console.log("Done downloading FOI requests!")
 console.rule("")
-console.log("Beginning to download messages")
-dload("https://fragdenstaat.de/api/v1/message/", "messages", "timestamp", cols_messages, console)
-console.log("Done downloading messages.")
+# console.log("Beginning to download messages")
+# dload("https://fragdenstaat.de/api/v1/message/", "messages", "timestamp", cols_messages, console)
+# console.log("Done downloading messages.")
 console.rule("")
-console.log("Beginning to download public bodies.")
-dload("https://fragdenstaat.de/api/v1/publicbody/", "public_bodies", "id", cols_pbodies, console)
-console.log("Done downloading public bodies.")
+# console.log("Beginning to download public bodies.")
+# dload("https://fragdenstaat.de/api/v1/publicbody/", "public_bodies", "id", cols_pbodies, console)
+# console.log("Done downloading public bodies.")
 dload("https://fragdenstaat.de/api/v1/campaign/", "campaigns", "start_date", cols_campaigns, console)
 console.rule("")
 console.log("Downloaded everything!")
